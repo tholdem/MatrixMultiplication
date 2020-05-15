6a182950f39414f58df253d49539fe169abb07e2
tholdem <jadenzwang@gmail.com>
2020-05-15 17:51:45 -0600

    fixing

--- LeastSquares/FISTA.m
+++ LeastSquares/FISTA.m
@@ -3,7 +3,6 @@ function [x, errorHistory] = FISTA(f,fgradient,g,varargin)
 %parse parameter inputs
 p = inputParser;
 addParameter(p,'errorThd',1e-6,@(x) x>0);
-%SRB: allow this to be an optional input (if none supplied, then do random by default; and I would use randn not rand, so that you get some negative values in there)
 addParameter(p,'initialPoint',0);
 addParameter(p,'maxIts',1e5,@(x) x>0);
 addParameter(p,'algo','leastsquares');
@@ -39,20 +38,20 @@ T = @(y,nablaY,L) y - 1/L*nablaY;
 %T = @(y,nablaY,L) prox(y,y-1/L*nablaY,g,1/L);
 t = 1;
 h = @(t) (1+sqrt(1+4*t^2))/2;
+x_old = zeros(size(x));
 
-fx = f(x);
-fx_old = fx + errorThd + 1;
 i=0;
 
-while fx - fx_old > errorThd && i < maxIts
+while i<1000
+    %norm(x_old - x) > errorThd && i < maxIts
     nablaY = fgradient(y);
-    %in least squares, L=sigma since while loop stops at k=1
-    L = backtrackingB3(f,T,y,nablaY,L_old,'errorThd',errorThd);
+    %set initial L_k=L_{k-1} and compute via backtracking
+    %L = backtrackingB3(f,T,y,nablaY,L_old,'errorThd',errorThd);
+    L = L_old;
     x_old = x;
-    fx_old = fx;
-    %update x,fx
-    x = T(y,nablaY,L);
-    fx = f(x);
+    %update x, which L to use? textbook example says 1/L but the original
+    %formula was just L, tried both and neither worked
+    x = T(y,nablaY,1/L);
     L_old = L;
     %update y
     t_old = t;
@@ -61,7 +60,7 @@ while fx - fx_old > errorThd && i < maxIts
 
     i=i+1;
     errorHistory(i)     = errFcn( x );
-    if ~mod( i, 5 )
+    if ~mod( i, 20 )
         fprintf('Iteration %5d, error is %.2e\n', i, errorHistory(i) );
     end
     
